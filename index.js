8c99eeeb1ae9a47bae83ec459e20a0dc5d382aa3
wayungi <fwayungi.com>
2023-10-04 10:50:29 +0300

    ADD: get products routes

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app =  express();
 const PORT = process.env.PORT || 3000
 
+const productDB = {
+    products: require('./model/product.json'),
+    setProducts: function(data) {
+        this.products = data
+    }
+}
 
+app.get('/', (req, res) => {
+    res.json({"products": productDB.products})
+})
 
 app.listen( PORT, console.log(`server is listening on port ${PORT}`))
 
