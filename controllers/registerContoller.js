b2cd5284a71f43e11a4b99039c6c2b05a92b563e
wayungi <fwayungi.com>
2023-10-04 16:07:02 +0300

    ADD: validation against registering duplicate username'

--- controllers/registerContoller.js
+++ controllers/registerContoller.js
@@ -11,6 +11,10 @@ const usersBD = {
 const registerUser = async (req, res) => {
     const {username, password} = req.body;
     if(!username || !password) return res.status(400).json({"Message": "username & password are erquired"});
+
+    const userExists = usersBD.users.find((user) => user.username === username);
+    if(userExists) return res.status(409).json({"message": `username ${username} is not avaialable`});
+    
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     const newUser = {
@@ -19,8 +23,6 @@ const registerUser = async (req, res) => {
         password: hashedPassword
     }
     usersBD.setUsers([...usersBD.users, newUser]);
-
-    console.log(usersBD.users)
     res.status(201).json(newUser)   ; 
 }
 
