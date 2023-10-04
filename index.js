633d90793bca93a762c17634723ef6004f255ca6
wayungi <fwayungi.com>
2023-10-04 11:21:30 +0300

    ADD: route to add new image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app =  express();
 const PORT = process.env.PORT || 3000
+const { v4: uuidv4 } = require('uuid');
 
 const productDB = {
     products: require('./model/product.json'),
@@ -9,8 +10,28 @@ const productDB = {
     }
 }
 
+
+//middleware
+app.use(express.json())
+// app.use(express.urlencoded())
+
 app.get('/', (req, res) => {
     res.json({"products": productDB.products})
+})
+
+app.post('/', (req, res) => {
+    const { name, image, price } = req. body
+    if(!name || !image || !price) return res.json({"error": "all fields are required"});
+    const newProduct = {
+        id: uuidv4(),
+        name,
+        image,
+        price
+    }
+  
+    productDB.setProducts([...productDB.products, newProduct])
+    console.log(productDB.products)
+    res.json({newProduct})
 })
 
 app.listen( PORT, console.log(`server is listening on port ${PORT}`))
