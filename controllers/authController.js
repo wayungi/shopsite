0abe6dc34312be1f626708d0487ca59351684c30
wayungi <fwayungi.com>
2023-10-05 10:17:30 +0300

    ADD: write registered users to file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config(); // 
+
 
 const usersBD = {
     users: require('../model/users.json'),
@@ -8,19 +10,29 @@ const usersBD = {
     }
 }
 
+
+
 const login = (req, res) => {
     const {username, password} = req.body;
     if(!username || !password) return res.status(401).json({"message": "Fill in username & password fields"});
 
-    console.log(usersBD.users)
     const currentUser = usersBD.users.find((user) => user.username === username);
-    console.log(currentUser)
     if(!currentUser) return res.status(401).json({"message": `username ${username} does not exist`})
 
     const userExist = bcrypt.compare(password, currentUser.password);
     if(!userExist) return res.status(401).json({"message": "Inavlid password"});
 
-    res.status(200).json({currentUser});
+    //when user exists, sign token
+    const accessToken = jwt.sign({username: currentUser.username}, process.env.ACCESS_TOKEN, { expiresIn: '30s' });
+    const refreshToken = jwt.sign({username: currentUser.username}, process.env.REFRESH_TOKEN, { expiresIn: '1d' }); // saved to db
+
+    //save refresh token with current user
+    const otherUsers =  usersBD.users.filter((user) => user.username !== currentUser.username);
+    const LoggedInUser = {...currentUser, refreshToken}
+    usersBD.setUsers([...otherUsers, LoggedInUser]);
+
+    res.cookie('jwt', refreshToken, {httpOnly: true, maxAge: 24*60*60*1000}); // 1 day
+    res.status(200).json({accessToken});
 };
 
 module.exports = { login };
--- controllers/registerContoller.js
+++ controllers/registerContoller.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const path =  require('path');
+
 
 const usersBD = {
     users: require('../model/users.json'),
@@ -10,10 +13,10 @@ const usersBD = {
 
 const registerUser = async (req, res) => {
     const {username, password} = req.body;
-    if(!username || !password) return res.status(400).json({"Message": "username & password are erquired"});
+    if(!username || !password) return res.status(400).json({"Message": "username & password are required"});
 
     const userExists = usersBD.users.find((user) => user.username === username);
-    if(userExists) return res.status(409).json({"message": `username ${username} is not avaialable`});
+    if(userExists) return res.status(409).json({"message": `username ${username} is already taken`});
     
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -23,7 +26,15 @@ const registerUser = async (req, res) => {
         password: hashedPassword
     }
     usersBD.setUsers([...usersBD.users, newUser]);
+
     console.log(usersBD.users)
+
+    //write the registered user to file
+    fs.writeFile( path.join(__dirname, '..', 'model', 'users.json'), JSON.stringify(usersBD.users), err => {
+        console.log(err)
+        }
+    )
+
     res.status(201).json(newUser)   ; 
 }
 
