e2ee93fc4401aaee963325c4c4f35620d0e88608
wayungi <fwayungi.com>
2023-10-05 21:33:29 +0300

    ADD: refresh route

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,8 +2,11 @@ const express =  require('express');
 const router = express.Router();
 const { registerUser } = require('../controllers/registerContoller');
 const { login } = require('../controllers/authController');
+// check the export vs the import alternative here
+const refreshTokenController = require('../controllers/refreshTokenCOntroller')
 
 router.post('/register',registerUser);
 router.post('/auth', login);
+router.get('/refresh', refreshTokenController.handleRefreshToken);
 
 module.exports = router;
