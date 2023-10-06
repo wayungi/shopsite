47b3880db5a6824da6327e48a4616b95c45c6a6a
wayungi <fwayungi.com>
2023-10-06 10:01:35 +0300

    ADD: verify middleware to verify id user is authorised to access endpoint

---
+++ middleware/verifyRoles.js
@@ -0,0 +1,23 @@
+/* NOTE: this middleware only terminates the request if its not allowed with Unauthhorized but lets the request
+ propagate if its allowed */
+
+const verifyRoles = (...allowedRoles) => {
+    return (req, res, next) => {
+        if(!req?.roles) return res.sendStatus(401); //Unauthorized
+        const rolesArray = [...allowedRoles]
+        console.log("Allowed roles")
+        console.log(rolesArray)
+        console.log("----------------")
+        console.log("Request roles: current user's roles")
+        console.log(req.roles);
+
+        /* map will return an array of true/false returned by the includes(), we the look through this array to find 
+        an instance of true which means the user is allowed to access this route */
+        const result = req.roles.map((role) => allowedRoles.includes(role)).find((bool) => bool === true)
+        if(!result) return res.sendStatus(401); //Unauthorized
+        next()
+
+    }
+};
+
+module.exports = verifyRoles
