188a6c8420938f2d723a30d7a73f3cd944270319
wayungi <fwayungi.com>
2023-10-19 11:25:33 +0300

    ADD: update db connection

--- config/dbCon.js
+++ config/dbCon.js
@@ -1,7 +1,19 @@
 const mongoose =  require('mongoose');
+
+//***************deployment on cloud
+// const connectDB =  async () => {
+//     try {
+//         await mongoose.connect(process.env.DATABASE_URI);// {useUnifiedTopology: true, useNewUrlParser: true}
+//     }catch (err) {
+//         console.log(err)
+//     }
+// };
+
+
+// ******************developoment on localhost
 const connectDB =  async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI);// {useUnifiedTopology: true, useNewUrlParser: true}
+        await mongoose.connect(process.env.DB_URI);// {useUnifiedTopology: true, useNewUrlParser: true}
     }catch (err) {
         console.log(err)
     }
